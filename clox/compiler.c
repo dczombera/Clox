ad1f5dd429ea3d3e6ee406e184f2bf67a1348741
Dennis Czombera <milchschaumschluerfer@gmail.com>
2020-05-13 14:00:57 +0200

    Compile for statements

--- clox/compiler.c
+++ clox/compiler.c
@@ -80,6 +80,7 @@ static void errorAt(Token* token, const char* message);
 static void errorAtCurrent(const char* message);
 static void expression();
 static void expressionStatement();
+static void forStatement();
 static ParseRule* getRule(TokenType type);
 static void grouping(bool canAssign);
 static uint8_t identifierConstant(Token* name);
@@ -332,6 +333,57 @@ static void expressionStatement() {
 	emitByte(OP_POP);
 }
 
+
+static void forStatement() {
+	beginScope();
+
+	consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+	if (match(TOKEN_SEMICOLON)) {
+		// No intializer.
+	}
+	else if (match(TOKEN_VAR)) {
+		varDeclaration();
+	}
+	else {
+		expressionStatement();
+	}
+
+	int loopStart = currentChunk()->count;
+
+	int exitJump = -1;
+	if (!match(TOKEN_SEMICOLON)) {
+		expression();
+		consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+		exitJump = emitJump(OP_JUMP_IF_FALSE);
+		emitByte(OP_POP); // Condition
+	}
+
+	if (!match(TOKEN_RIGHT_PAREN)) {
+		int bodyJump = emitJump(OP_JUMP);
+
+		int incrementStart = currentChunk()->count;
+		expression();
+		emitByte(OP_POP);
+		consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clause.");
+
+		emitLoop(loopStart);
+		loopStart = incrementStart;
+		patchJump(bodyJump);
+	}
+
+	statement();
+
+	emitLoop(loopStart);
+
+	if (exitJump != -1) {
+		patchJump(exitJump);
+		emitByte(OP_POP);
+	}
+
+	endScope();
+}
+
 static uint8_t identifierConstant(Token* token) {
 	return makeConstant(OBJ_VAL(copyString(token->start, token->length)));
 }
@@ -399,6 +451,9 @@ static void statement() {
 	}
 	else if (match(TOKEN_WHILE)) {
 		whileStatement();
+	}
+	else if (match(TOKEN_FOR)) {
+		forStatement();
 	}
 	else if (match(TOKEN_LEFT_BRACE)) {
 		beginScope();
